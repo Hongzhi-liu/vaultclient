88f9c2de49840f17079b4fabf1eeff21299213bd
Peter Adams <peter.adams1014@gmail.com>
2019-12-12 14:27:28 +1000

    WIP sphere rendering

--- src/vcQuadTree.h
+++ src/vcQuadTree.h
@@ -51,7 +51,7 @@ struct vcQuadTreeNode
   bool rendered;
 
   // cached
-  udDouble2 worldBounds[4]; // corners
+  udDouble3 worldBounds[4]; // corners
   udDouble2 tileCenter, tileExtents;
 
   vcNodeRenderInfo renderInfo;
